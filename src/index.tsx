19cb95f28048bdf1d3e3e3897079d30f185fa004
KoinoEngineering <email@koino.engineering>
2019-04-29 08:29:28 +0900

    #8 typescriptをindex.tsxに統合

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,48 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import { withStyles, createStyles, Button } from "@material-ui/core";
+import { ButtonProps } from "@material-ui/core/Button"
 
-ReactDOM.render(<App />, document.getElementById('root'));
+
+/** Components **/
+/** BaseButton 後々のことを考えていったんラップしてみる **/
+export type BaseButtonProp = Partial<
+    ButtonProps
+>;
+
+class BaseButton extends React.Component<BaseButtonProp>{
+    render() {
+        const { id, value, children } = this.props;
+        return <Button id={id} value={value}>{children}</Button>;
+    }
+}
+
+
+/** Calclator 計算機本体 **/
+const CalcStyles = createStyles({
+
+})
+
+interface CalclateProp {
+
+}
+
+class CalclateComponent extends React.Component<CalclateProp> {
+    render() {
+        return (
+            <div>
+                <div>
+                    displayArea
+                </div>
+                <div>
+                    <BaseButton>TEST</BaseButton>
+                </div>
+            </div>
+        );
+    }
+}
+
+const Calclator = withStyles(CalcStyles)(CalclateComponent);
+
+ReactDOM.render(<Calclator />, document.getElementById('root'));
